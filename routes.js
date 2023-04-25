a3cc4f964c638e05e607e18eb93371c698f01180
Ajay Savaliya <ajay.savaliya@techypanther.com>
2023-04-25 14:02:54 +0530

    changed in routes

--- routes.js
+++ routes.js
@@ -55,7 +55,7 @@ import EditMeetingSubjects from './src/screens/servicesScreen/meetings/editMeeti
 import YourAnswer from './src/screens/servicesScreen/meetings/yourAnswer/YourAnswer';
 import Subjects from './src/screens/servicesScreen/Subjects';
 import AddDraftSubject from './src/screens/servicesScreen/subjects/addDraftSybject/AddDraftSubject';
-import EditAppointmentDateAndTime from './src/screens/servicesScreen/appointments/EditAppointmentDateAndTime /EditAppointmentDateAndTime';
+import EditAppointmentDateAndTime from './src/screens/servicesScreen/appointments/EditAppointmentDateAndTime/EditAppointmentDateAndTime';
 import EditAppointmentGeneral from './src/screens/servicesScreen/appointments/EditAppointmentGeneral/EditAppointmentGeneral';
 import EditAppointmentLocation from './src/screens/servicesScreen/appointments/EditAppointmentLocation/EditAppointmentLocation';
 import EditAppointmentUsers from './src/screens/servicesScreen/appointments/EditAppointmentUsers/EditAppointmentUsers';
